cf43a733f4a0135883d35c72b744fda4d9a2906f
Niki-bic <ic19b013@technikum-wien.at>
2020-12-02 11:57:20 +0100

    noch ein seg fault, aber das Auslesen der Daten scheint schon mal grundlegend zu passen

--- simple_message_client.c
+++ simple_message_client.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <netdb.h>
 #include "simple_message_client_commandline_handling.h" // mus noch zu <simple...> geändert werden
@@ -12,6 +13,7 @@
 
 
 void usage(FILE *stream, const char *cmnd, int exitcode);
+int handle_reply(char *reply);
 
 
 int main(const int argc, const char * const*argv) {
@@ -28,23 +30,40 @@ int main(const int argc, const char * const*argv) {
     struct addrinfo *result = NULL;
     struct addrinfo *res_ptr = NULL;
 
-    int socket_fd_write = -1;
-    int socket_fd_read = -1;
+    int socket_write = -1;
+    int socket_read = -1;
 
-    FILE *file_ptr_write = NULL;
-    FILE *file_ptr_read = NULL;
+    FILE *file_write = NULL;
+    FILE *file_read = NULL;
 
-    char request[BUF_LEN] = {'\0'};
+    int request_len = 0;
+    char *request = NULL;
+    // char request[BUF_LEN] = {'\0'};
     char reply[BUF_LEN] = {'\0'}; // ws kein char-Buffer sondern besser was anderes, sind ja gemischte Daten
+
+    request_len = strlen("user=") + strlen(user) + 1 + strlen(message) + 2;
 
     // eventuell in eine Funktion auslagern und error-checking, sowie strncpy stats strcpy sowie strncat statt strcat
     if (img_url == NULL) {
+        request = (char *) calloc(request_len, sizeof(char));
+        if (request == NULL) {
+            // error
+            exit(EXIT_FAILURE);
+        }
+
         strcpy(request, "user=");
         strcat(request, user);
         strcat(request, "\n");
         strcat(request, message);
         strcat(request, "\n"); // unbedingt notwendig
     } else { // img_url != NULL
+        request_len += strlen("img=") + strlen(img_url) + 1;
+        request = (char *) calloc(request_len, sizeof(char));
+        if (request == NULL) {
+            // error
+            exit(EXIT_FAILURE);
+        }
+
         strcpy(request, "user=");
         strcat(request, user);
         strcat(request, "\n");
@@ -55,26 +74,33 @@ int main(const int argc, const char * const*argv) {
         strcat(request, "\n"); // unbedingt notwendig
     }
 
+    // printf("%d", request_len);
+    // fprintf(stderr, "%s", request);
+
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_UNSPEC; // egal ob IP4 oder IP6
     hints.ai_socktype = SOCK_STREAM; // TCP
 
     if (getaddrinfo(server, port, &hints, &result) != 0) {
         // error
+        exit(EXIT_FAILURE);
     }
 
     for (res_ptr = result; res_ptr != NULL; res_ptr = res_ptr->ai_next) {
-        socket_fd_write = socket(res_ptr->ai_family, res_ptr->ai_socktype, res_ptr->ai_protocol);
+        socket_write = socket(res_ptr->ai_family, res_ptr->ai_socktype, res_ptr->ai_protocol);
 
-        if (socket_fd_write == -1) {
+        if (socket_write == -1) {
             continue;
         }
 
-        if (connect(socket_fd_write, res_ptr->ai_addr, res_ptr->ai_addrlen) != -1) {
+        if (connect(socket_write, res_ptr->ai_addr, res_ptr->ai_addrlen) != -1) {
             break;
         }
 
-        close(socket_fd_write);
+        if (close(socket_write) == -1) {
+            // error
+            exit(EXIT_FAILURE);
+        }
     }
 
     freeaddrinfo(result);
@@ -84,19 +110,18 @@ int main(const int argc, const char * const*argv) {
         exit(EXIT_FAILURE);
     }
 
-    if ((socket_fd_read = dup(socket_fd_write)) == -1) {
-        // error
+    if ((socket_read = dup(socket_write)) == -1) {
+        // error in dup()
         exit(EXIT_FAILURE);
     }
 
-    if ((file_ptr_write = fdopen(socket_fd_write, "w")) == NULL) {
-        // error
-        fprintf(stderr, "file_ptr_write ist NULL\n");
+    if ((file_write = fdopen(socket_write, "w")) == NULL) {
+        // error in fdopen
         exit(EXIT_FAILURE);
     }
 
-    if ((file_ptr_read = fdopen(socket_fd_read, "rb")) == NULL) {
-        // error
+    if ((file_read = fdopen(socket_read, "rb")) == NULL) {
+        // error in fdopen
         exit(EXIT_FAILURE);
     }
 
@@ -104,37 +129,46 @@ int main(const int argc, const char * const*argv) {
     // setvbuf(file_ptr_write, request, _IOLBF, sizeof(request));
     // setvbuf(file_ptr_read, reply, _IOLBF, sizeof(reply));
 
-    fprintf(file_ptr_write, "%s", request); // oder vl besser fputs()
-
-    if (fflush(file_ptr_write) != 0) { // 
-        // error
+    if (fprintf(file_write, "%s", request) < 0) {
+        // error fprintf
+        exit(EXIT_FAILURE);
     }
 
-    if (shutdown(fileno(file_ptr_write), SHUT_WR) == -1) { // sendet offenbar EOF
+    if (fflush(file_write) != 0) { // 
         // error
+        exit(EXIT_FAILURE);
+    }
+
+    free(request);
+
+    if (shutdown(fileno(file_write), SHUT_WR) == -1) { // sendet offenbar EOF
+        // error
+        exit(EXIT_FAILURE);
     }
 
     // einlesen des reply
     int i = 0;
-    while (fread(&reply[i * 100], sizeof(reply[0]), 100, file_ptr_read) != 0) {
+    while (fread(&reply[i * 100], sizeof(reply[0]), 100, file_read) != 0) {
         i++;
     }
+
+    int status = handle_reply(reply);
     printf("%s", reply);
 
-    if (fclose(file_ptr_read) == -1) {
-        // error
+    if (fclose(file_read) == -1) {
+        // error        
     }
     
-    if (fclose(file_ptr_write) == -1) {
+    if (fclose(file_write) == -1) {
         // error
     }
 
     // close() zweimal
-    if (close(socket_fd_read) == -1) {
+    if (close(socket_read) == -1) {
         // error
     }
 
-    if (close(socket_fd_write) == -1) {
+    if (close(socket_write) == -1) {
         // error
     }
 
@@ -153,3 +187,73 @@ void usage(FILE *stream, const char *cmnd, int exitcode) {
     -h, --help\n", cmnd);
     exit(exitcode);
 } // end usage()
+
+
+int handle_reply(char *reply) {
+    fprintf(stderr, "%s\n", reply);
+    fprintf(stderr, "\n\n");
+    int status = 0;
+    int len = 0;
+    char *pointer = reply;
+    char filename[BUF_LEN] = {'\0'}; // gefällt mir noch nicht ganz
+    FILE *file = NULL;
+
+    // search for status
+    // dann in einer Schleife file (= filename) und len auslesen, und eine file mit Namen 
+    // filename erstellen und den content (d.h. die nächsten len-Bytes) reinschreiben.
+
+
+    if ((pointer = strstr(reply, "status=")) == NULL) {
+        // error: status= not found
+    }
+    pointer += 7;
+    status = strtol(pointer, NULL, 10); // erro-checking noch einbauen
+
+
+
+    while (pointer != NULL) {
+        if ((pointer = strstr(pointer, "file=")) == NULL) {
+            // error: no file=
+        }
+        pointer += 5; // strlen("file=") == 5
+        int i = 0;
+        memset(filename, 0, BUF_LEN);
+        while(*pointer != '\n') {
+            filename[i] = *pointer;
+            i++;
+            pointer++;
+        }
+
+
+        /* if ((pointer = strstr(pointer, "len=")) == NULL) {
+            // error: no len=
+        } */
+        pointer++;       // newline-Zeichen schlucken
+        pointer += 4;    // strlen("len=") == 4
+        len = strtol(pointer, NULL, 10); // error-checking noch einbauen
+
+
+        while (*pointer != '\n') { // Längenangabe überspringen
+            pointer++;
+        }
+        pointer++; // newline-Zeichen schlucken
+
+        if ((file = fopen(filename, "w")) == NULL) {
+            // error in fopen
+        }
+      
+        if (fwrite(pointer, sizeof(char), len, file) < len) {
+            // error in fwrite
+        }
+
+        if (fclose(file) == -1) {
+            // error in fclose
+        }
+
+        fprintf(stderr, "status=%d, file=%s, len=%d\n", status, filename, len);
+
+    } // end while
+
+
+    return status;
+} // end handle_reply()

a1151b60fb1f7d32326ec5d8ba17e6a7bf698e0c
Niki-bic <ic19b013@technikum-wien.at>
2020-12-05 14:57:14 +0100

    mal ein bisschen überarbeitet, eventuell wird noch was ausgelagert

--- simple_message_client.c
+++ simple_message_client.c
@@ -9,181 +9,197 @@
 // -h einbauen -- DONE
 // das Einlesen des reply noch verbesser (mit Hinblick auf die Testcases)
 
-
 #include <errno.h>
+#include <limits.h>
 #include <netdb.h>
 #include <simple_message_client_commandline_handling.h>
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <errno.h>
-
 
 #define BUF_LEN (1024 * 1024) // noch überlegen wie lang notwendig
+#define REPLY_ERROR -3l
 
+const char *cmd = NULL; // globaler Speicher für argv[0]
 
-void usage(FILE *stream, const char *cmnd, int exitcode);
-long handle_reply(char *reply);
+static long handle_reply(const char * const reply);
+static void remove_resources(int socket_read, int socket_write, FILE *file_read,
+                             FILE *file_write, int exit_status);
+static long strtol_e(const char *const string);
+static void usage(FILE *stream, const char *cmnd, int exitcode);
 
+int main(const int argc, const char *const *argv)
+{
+    cmd = argv[0];
 
-int main(const int argc, const char * const *argv) {
-    const char *server  = NULL;
-    const char *port    = NULL;
-    const char *user    = NULL;
+    const char *server = NULL;
+    const char *port = NULL;
+    const char *user = NULL;
     const char *message = NULL;
     const char *img_url = NULL;
-    int verbose         = FALSE;
+    int verbose = FALSE;
 
     smc_parsecommandline(argc, argv, &usage, &server, &port, &user, &message, &img_url, &verbose);
 
-    if (verbose != FALSE) {           // -h
+    if (verbose == TRUE)
+    {
         usage(stdout, argv[0], EXIT_SUCCESS);
     }
 
     struct addrinfo hints;
-    struct addrinfo *result  = NULL;
+    struct addrinfo *result = NULL;
     struct addrinfo *res_ptr = NULL;
 
     int socket_write = -1;
-    int socket_read  = -1;
-
-    int status = -1;
+    int socket_read = -1;
 
     FILE *file_write = NULL;
-    FILE *file_read  = NULL;
+    FILE *file_read = NULL;
+
+    long status = -1;
 
     char reply[BUF_LEN] = {'\0'};
 
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family   = AF_UNSPEC;             // egal ob IP4 oder IP6
-    hints.ai_socktype = SOCK_STREAM;           // TCP
+    hints.ai_family = AF_UNSPEC;     // egal ob IP4 oder IP6
+    hints.ai_socktype = SOCK_STREAM; // TCP
 
     int s = getaddrinfo(server, port, &hints, &result);
-	if(s != 0){
-           fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-           exit(EXIT_FAILURE);
-	}
-    
-    for (res_ptr = result; res_ptr != NULL; res_ptr = res_ptr->ai_next) {
+    if (s != 0)
+    {
+        fprintf(stderr, "%s: getaddrinfo: %s\n", cmd, gai_strerror(s));
+        return EXIT_FAILURE;
+    }
+
+    for (res_ptr = result; res_ptr != NULL; res_ptr = res_ptr->ai_next)
+    {
         socket_write = socket(res_ptr->ai_family, res_ptr->ai_socktype, res_ptr->ai_protocol);
 
-        if (socket_write == -1) {
+        if (socket_write == -1)
+        {
             continue;
         }
 
-        if (connect(socket_write, res_ptr->ai_addr, res_ptr->ai_addrlen) != -1) {
+        if (connect(socket_write, res_ptr->ai_addr, res_ptr->ai_addrlen) != -1)
+        {
             break;
         }
-    
-    	errno = 0;
-		
-        if (close(socket_write) == -1) {
-            fprintf(stderr, "Error closing write socket: %s\n", strerror(errno));
-            exit(EXIT_FAILURE);
-        }
+
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     freeaddrinfo(result);
- 
-    if (res_ptr == NULL) {
-        fprintf(stderr, "Error: Client failed to connect\n");
-        // error: client failed to connect
-        exit(EXIT_FAILURE);
-    }
-    
-    errno = 0;
 
-    if ((socket_read = dup(socket_write)) == -1) {
-        fprintf(stderr, "Error duplicating file discriptor: %s\n", strerror( errno ));
-        exit(EXIT_FAILURE);
+    if (res_ptr == NULL)
+    {
+        fprintf(stderr, "%s: Error: Client failed to connect\n", cmd);
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     errno = 0;
 
-    if ((file_write = fdopen(socket_write, "w")) == NULL) {
-
-        fprintf(stderr, "Error opening write socket: %s\n", strerror(errno));
-        // error in fdopen
-        exit(EXIT_FAILURE);
+    if ((socket_read = dup(socket_write)) == -1)
+    {
+        fprintf(stderr, "%s: Error duplicating file descriptor: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     errno = 0;
 
-    if ((file_read = fdopen(socket_read, "r")) == NULL) {
+    if ((file_write = fdopen(socket_write, "w")) == NULL)
+    {
+        fprintf(stderr, "%s: Error opening file: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
+    }
 
-        fprintf(stderr, "Error opening read socket: %s\n", strerror(errno));
-        // error in fdopen
-        exit(EXIT_FAILURE);
+    errno = 0;
+
+    if ((file_read = fdopen(socket_read, "r")) == NULL)
+    {
+        fprintf(stderr, "%s: Error opening file: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     // line-buffering einstellen?
     // setvbuf(file_ptr_write, request, _IOLBF, sizeof(request));
     // setvbuf(file_ptr_read, reply, _IOLBF, sizeof(reply));
 
+    errno = 0;
 
     // sending request
-    if (img_url == NULL) {
-	errno = 0;
-        if (fprintf(file_write, "user=%s\n%s\n", user, message) < 0) {
-            fprintf(stderr, "Error writing in stream: %s\n", strerror(errno)); // error fprintf
-            exit(EXIT_FAILURE);
-        }
-    } else { // img_url != NULL
-	errno = 0;
-        if (fprintf(file_write, "user=%s\nimg=%s\n%s\n", user, img_url, message) < 0) {
-            fprintf(stderr, "Error writing in stream: %s\n", strerror(errno));
-            // error fprintf
-            exit(EXIT_FAILURE);
+    if (img_url == NULL)
+    {
+        if (fprintf(file_write, "user=%s\n%s\n", user, message) < 0)
+        {
+            fprintf(stderr, "%s: Error writing in stream: %s\n", cmd, strerror(errno)); // error fprintf
+            remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
         }
     }
-
-    errno = 0;
-
-    if (fflush(file_write) != 0) { // 
-        fprintf(stderr, "Error flushing write stream: %s\n", strerror(errno));// error
-        exit(EXIT_FAILURE);
+    else // img_url != NULL
+    {
+        if (fprintf(file_write, "user=%s\nimg=%s\n%s\n", user, img_url, message) < 0)
+        {
+            fprintf(stderr, "%s: Error writing in stream: %s\n", cmd, strerror(errno));
+            remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
+        }
     }
 
     errno = 0;
 
-    if (shutdown(fileno(file_write), SHUT_WR) == -1) { // sendet offenbar EOF
-        fprintf(stderr, "Error shutdown write socket failed: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+    if (fflush(file_write) != 0)
+    {
+        fprintf(stderr, "%s: Error flushing write stream: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
-    
+
     errno = 0;
 
-    if (fclose(file_write) == -1) {
-        fprintf(stderr, "Error closing write socket failed: %s\n", strerror(errno));
+    if (shutdown(fileno(file_write), SHUT_WR) == -1)
+    {
+        fprintf(stderr, "%s: Error shutdown write socket failed: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
+    }
+
+    errno = 0;
+
+    if (fclose(file_write) == -1)
+    {
+        fprintf(stderr, "%s: Error closing file failed: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     // einlesen des reply // bis hierher kommt er bei TESTCASE=3
-    // int i = 0;
-    // while (fread(&reply[i * 100], sizeof(reply[0]), 100, file_read) != 0 && i < BUF_LEN) {
-    //     i++;
-    // }
-    recv(socket_read, reply, BUF_LEN, 0);
+    int i = 0;
+    while (fread(&reply[i * 100], sizeof(reply[0]), 100, file_read) != 0 && i < BUF_LEN)
+    {
+        i++;
+    }
 
-    status = (int) handle_reply(reply);
-    
+    status = handle_reply(reply);
+    if (status == REPLY_ERROR)
+    {
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
+    }
+
     errno = 0;
 
-    if (fclose(file_read) == -1) {
-        fprintf(stderr, "Error closing read socket failed: %s\n", strerror(errno));
+    if (fclose(file_read) == -1)
+    {
+        fprintf(stderr, "%s: Error closing file failed: %s\n", cmd, strerror(errno));
+        remove_resources(socket_read, socket_write, file_read, file_write, EXIT_FAILURE);
     }
 
     // close nicht notwendig, da fclose() auch den
     // darunter liegenden Deskriptor schließt (man-page)
 
-    return status;
+    return (long)status;
 } // end main()
 
-
-void usage(FILE *stream, const char *cmnd, int exitcode) {
+static void usage(FILE *stream, const char *cmnd, int exitcode)
+{
     fprintf(stream, "usage: %s options\noptions:\n\t\
     -s, --server <server>   full qualified domain name or IP address of the server\n\t\
     -p, --port <port>       well-known port of the server [0..65535]\n\t\
@@ -191,104 +207,169 @@ void usage(FILE *stream, const char *cmnd, int exitcode) {
     -i, --image <URL>       URL pointing to an image of the posting user\n\t\
     -m, --message <message> message to be added to the bulletin board\n\t\
     -v, --verbose           verbose output\n\t\
-    -h, --help\n", cmnd);
+    -h, --help\n",
+            cmnd);
     exit(exitcode);
 } // end usage()
 
-
 // eventuell split_input aus simple_message_server_logic abkupfern
-long handle_reply(char *reply) {
-    long status       = 0;
+static long handle_reply(const char * const reply)
+{
+    long status = 0;
     unsigned long len = 0;
-    char *pointer     = reply;
-    FILE *file        = NULL;
+    char *pointer = (char *)reply;
+    FILE *file = NULL;
     char filename[BUF_LEN] = {'\0'}; // gefällt mir noch nicht ganz
 
-
     // search for status
-    // dann in einer Schleife file (= filename) und len auslesen, und eine file mit Namen 
+    // dann in einer Schleife file (= filename) und len auslesen, und eine file mit Namen
     // filename erstellen und den content (d.h. die nächsten len-Bytes) reinschreiben.
 
-
-    if ((pointer = strstr(reply, "status=")) == NULL) {
-	    fprintf(stderr, "Error 'status=' not found\n");
-        return -1;
-        // error: status= not found
-    }
-    pointer += 7;                       // strlen("status=") == 7
-    errno = 0;
-    status = strtol(pointer, NULL, 10); // erro-checking noch einbauen
-    
-    if (pointer == NULL){
-        fprintf (stderr, "number : %lu invalid (no digits found, 0 returned)\n", status);
-    } else if (errno == ERANGE && status == LONG_MIN) {
-        fprintf (stderr, "number : %lu invalid (underflow occurred)\n", status);
-    } else if (errno == ERANGE && status == LONG_MAX) {
-        fprintf (stderr, "number : %lu invalid (overflow occurred)\n", status);
-    } else if (errno == EINVAL) {
-        fprintf (stderr, "number : %lu invalid (base contains unsupported value)\n", status);
-    } else if (errno != 0 && status == 0) {
-        fprintf (stderr, "number : %lu invalid (unspecified error occurred)\n", status);
+    if ((pointer = strstr(reply, "status=")) == NULL)
+    {
+        fprintf(stderr, "%s: Error 'status=' not found\n", cmd);
+        return REPLY_ERROR;
     }
 
+    pointer += 7; // strlen("status=") == 7
+    status = strtol_e(pointer);
+    if (status == REPLY_ERROR)
+    {
+        fprintf(stderr, "%s: Error: strtol failed\n", cmd);
+        return REPLY_ERROR;
+    }
 
-    while (TRUE) {
-        if ((pointer = strstr(pointer, "file=")) == NULL) {
-            // error: no file=
-            break; // das ist wichtig!
+    while (TRUE)
+    {
+        if ((pointer = strstr(pointer, "file=")) == NULL)
+        {
+            break;
         }
+
         pointer += 5; // strlen("file=") == 5
         int i = 0;
         memset(filename, 0, BUF_LEN);
-        while (*pointer != '\n') {
+        while (*pointer != '\n')
+        {
             filename[i] = *pointer;
             i++;
             pointer++;
         }
-        pointer++;                       // newline-Zeichen schlucken
+        pointer++; // newline-Zeichen schlucken
 
-
-        pointer += 4;                    // strlen("len=") == 4
-		errno = 0;
-        len = strtol(pointer, NULL, 10); // error-checking noch einbauen
-	
-		if (pointer == NULL) {
-            fprintf (stderr, "number : %lu invalid (no digits found, 0 returned)\n", len);
-		} else if (errno == ERANGE && len == 0) {
-            fprintf (stderr, "number : %lu invalid (underflow occurred)\n", len);
-		} else if (errno == ERANGE && len == ULONG_MAX) {
-            fprintf (stderr, "number : %lu invalid (overflow occurred)\n", len);
-		} else if (errno == EINVAL){
-            fprintf (stderr, "number : %lu invalid (base contains unsupported value)\n", len);
-    	} else if (errno != 0 && len == 0) {
-            fprintf (stderr, "number : %lu invalid (unspecified error occurred)\n", len);
+        pointer += 4; // strlen("len=") == 4
+        errno = 0;
+        len = strtol_e(pointer);
+        if ((long)len == REPLY_ERROR)
+        {
+            fprintf(stderr, "%s: Error: strtol failed\n", cmd);
+            return REPLY_ERROR;
         }
-	
-        while (*pointer != '\n') {       // Längenangabe überspringen
+
+        while (*pointer != '\n') // Längenangabe überspringen
+        {
             pointer++;
         }
-        pointer++;                       // newline-Zeichen schlucken
+        pointer++; // newline-Zeichen schlucken
 
-		errno = 0;
-	
-        if ((file = fopen(filename, "w")) == NULL) {
-            fprintf(stderr, "Error opening file %s: %s\n", filename, strerror(errno));
-        }
-        
         errno = 0;
-        if (fwrite(pointer, sizeof(char), len, file) < len) {
-            fprintf(stderr, "Error writing in file %s: %s\n", filename, strerror(errno)); // error in fwrite
-        }
-	
-	errno = 0;
 
-        if (fclose(file) == -1) {
-            fprintf(stderr, "Error closing file %s: %s\n", filename, strerror(errno));
+        if ((file = fopen(filename, "w")) == NULL)
+        {
+            fprintf(stderr, "%s: Error opening file %s: %s\n", cmd, filename, strerror(errno));
+            return REPLY_ERROR;
+        }
+
+        errno = 0;
+
+        if (fwrite(pointer, sizeof(char), len, file) < len)
+        {
+            fprintf(stderr, "%s: Error writing in file %s: %s\n", cmd, filename, strerror(errno));
+            return REPLY_ERROR;
+        }
+
+        errno = 0;
+
+        if (fclose(file) == -1)
+        {
+            fprintf(stderr, "%s: Error closing file %s: %s\n", cmd, filename, strerror(errno));
+            return REPLY_ERROR;
         }
     } // end while
-
 
     return status;
 } // end handle_reply()
 
+static long strtol_e(const char *const string)
+{
+    char *end_ptr;
+    errno = 0;
+    long number = strtol(string, &end_ptr, 10);
 
+    if (string == NULL)
+    {
+        fprintf(stderr, "%s: number : %lu invalid (no digits found, 0 returned)\n", cmd, number);
+        return REPLY_ERROR;
+    }
+    else if (errno == ERANGE && number == 0)
+    {
+        fprintf(stderr, "%s: number : %lu invalid (underflow occurred)\n", cmd, number);
+        return REPLY_ERROR;
+    }
+    else if (errno == ERANGE && number == LONG_MAX)
+    {
+        fprintf(stderr, "%s: number : %lu invalid (overflow occurred)\n", cmd, number);
+        return REPLY_ERROR;
+    }
+    else if (errno == EINVAL)
+    {
+        fprintf(stderr, "%s: number : %lu invalid (base contains unsupported value)\n", cmd, number);
+        return REPLY_ERROR;
+    }
+    else if (errno != 0 && number == 0)
+    {
+        fprintf(stderr, "%s: number : %lu invalid (unspecified error occurred)\n", cmd, number);
+        return REPLY_ERROR;
+    }
+
+    return number;
+} // end strtol_e()
+
+static void remove_resources(int socket_read, int socket_write, FILE *file_read, FILE *file_write, int exit_status)
+{
+    if (file_read != NULL)
+    {
+        if (fclose(file_read) != 0)
+        {
+            fprintf(stderr, "%s: Error while closing file\n", cmd);
+        }
+        socket_read = -1;
+    }
+
+    if (file_write != NULL)
+    {
+        if (fclose(file_write) != 0)
+        {
+            fprintf(stderr, "%s: Error while closing file\n", cmd);
+        }
+        socket_write = -1;
+    }
+
+    if (socket_read != -1)
+    {
+        if (close(socket_read) != 0)
+        {
+            fprintf(stderr, "%s: Error while closing socket\n", cmd);
+        }
+    }
+
+    if (socket_write != -1)
+    {
+        if (close(socket_write) != 0)
+        {
+            fprintf(stderr, "%s: Error while closing socket\n", cmd);
+        }
+    }
+
+    exit(exit_status);
+} // end remove_resources
